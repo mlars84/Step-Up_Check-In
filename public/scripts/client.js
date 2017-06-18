82c530730a6e3142645adccbe3992bb00a958eaa
Matt Larson <matt.a.larson@gmail.com>
2017-06-18 13:03:48 -0500

    indented client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,18 +3,18 @@ var googleAuthApp = angular.module('theGoogles', ['ngRoute']);
 googleAuthApp.config(['$routeProvider', function ($routeProvider) {
 
   $routeProvider
-    .when('/calendar', {
-      templateUrl: '/public/views/templates/calendar.html',
-      controller: 'CalendarController',
-      controllerAs: 'calendar',
-    })
-    .when('/login', {
-      templateUrl: '/public/views/templates/login.html',
-      controller: 'AuthController',
-      controllerAs: 'auth',
-    })
-    .otherwise({
-      redirectTo: 'login',
-    });
+  .when('/calendar', {
+    templateUrl: '/public/views/templates/calendar.html',
+    controller: 'CalendarController',
+    controllerAs: 'calendar',
+  })
+  .when('/login', {
+    templateUrl: '/public/views/templates/login.html',
+    controller: 'AuthController',
+    controllerAs: 'auth',
+  })
+  .otherwise({
+    redirectTo: 'login',
+  });
 },
 ]);
