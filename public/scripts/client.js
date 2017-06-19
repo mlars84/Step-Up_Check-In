90b87ec77d6de489265ec27f4749ecd3c393bb62
Matt Larson <matt.a.larson@gmail.com>
2017-06-19 16:44:37 -0500

    updated angular routing and added corresponding controllers and services

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,10 +3,25 @@ var googleAuthApp = angular.module('theGoogles', ['ngRoute']);
 googleAuthApp.config(['$routeProvider', function ($routeProvider) {
 
   $routeProvider
-  .when('/adminhome', {
+  .when('/admin-home', {
     templateUrl: '/public/views/templates/admin-home.html',
     controller: 'adminHomeController',
     controllerAs: 'AHC',
+  })
+  .when('admin-intern', {
+    templateUrl: '/public/views/templates/admin-intern.html',
+    controller: 'adminInternController',
+    controllerAs: 'AIC',
+  })
+  .when('updateQuestions', {
+    templateUrl: '/public/views/templates/updateQuestions.html',
+  controller: 'updateQuestionsController',
+    controllerAs: 'UQC',
+  })
+  .when('importExport', {
+    templateUrl: '/public/views/templates/importExport.html',
+  controller: 'importExportController',
+    controllerAs: 'IEC',
   })
   .when('/calendar', {
     templateUrl: '/public/views/templates/calendar.html',
@@ -17,6 +32,11 @@ googleAuthApp.config(['$routeProvider', function ($routeProvider) {
     templateUrl: '/public/views/templates/login.html',
     controller: 'AuthController',
     controllerAs: 'auth',
+  })
+  .when('/feedbackform', {
+    templateUrl: '/public/views/templates/feedbackform.html',
+    controller: 'feedBackFormController',
+    controllerAs: 'FBFC',
   })
   .otherwise({
     redirectTo: 'login',
---
+++ public/scripts/controllers/adminInternController.js
@@ -0,0 +1,5 @@
+googleAuthApp.controller('adminInternController', ['adminInternService', function ($http, adminInternService) {
+  console.log('loaded adminInternController');
+  const self = this;
+
+}]); //end adminInternController
