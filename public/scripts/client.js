64783858ee48e79b5807fd67a8ac8b0401611b09
Matt Larson <matt.a.larson@gmail.com>
2017-06-20 09:50:10 -0500

    nav bar connected and all ctlrs and services ready to go

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,37 +1,33 @@
 var googleAuthApp = angular.module('theGoogles', ['ngRoute']);
 
-googleAuthApp.config(['$routeProvider', function ($routeProvider) {
+googleAuthApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  // $locationProvider.html5Mode(true);
 
   $routeProvider
+  .when('/login', {
+    templateUrl: '/public/views/templates/login.html',
+    controller: 'AuthController',
+    controllerAs: 'auth',
+  })
   .when('/admin-home', {
     templateUrl: '/public/views/templates/admin-home.html',
     controller: 'adminHomeController',
     controllerAs: 'AHC',
   })
-  .when('admin-intern', {
+  .when('/admin-intern', {
     templateUrl: '/public/views/templates/admin-intern.html',
     controller: 'adminInternController',
     controllerAs: 'AIC',
   })
-  .when('updateQuestions', {
+  .when('/updateQuestions', {
     templateUrl: '/public/views/templates/updateQuestions.html',
   controller: 'updateQuestionsController',
     controllerAs: 'UQC',
   })
-  .when('importExport', {
+  .when('/importExport', {
     templateUrl: '/public/views/templates/importExport.html',
   controller: 'importExportController',
     controllerAs: 'IEC',
-  })
-  .when('/calendar', {
-    templateUrl: '/public/views/templates/calendar.html',
-    controller: 'CalendarController',
-    controllerAs: 'calendar',
-  })
-  .when('/login', {
-    templateUrl: '/public/views/templates/login.html',
-    controller: 'AuthController',
-    controllerAs: 'auth',
   })
   .when('/feedbackform', {
     templateUrl: '/public/views/templates/feedbackform.html',
@@ -39,7 +35,7 @@ googleAuthApp.config(['$routeProvider', function ($routeProvider) {
     controllerAs: 'FBFC',
   })
   .otherwise({
-    redirectTo: 'login',
+    redirectTo: '/login',
   });
 },
 ]);
