2196b626cd84d68e184cec81a0c884cca1054890
Tessa Ganser <t.m.ganser@gmail.com>
2017-06-21 09:41:47 -0500

    tessa removed excess old routes

--- routes/private/index.js
+++ routes/private/index.js
@@ -5,33 +5,22 @@
  */
 var express = require('express');
 var router  = express.Router();
-// var calendar = require('./calendar');
-// var adminhome = require('./adminhome');
-// var interns = require('./adminintern');
-// var updateq = require('./updateq');
-// var importexport = require('./importexport');
-// var feedback = require('./feedback');
 
 
 var questions = require('./questions');
-var responses = require('./responses');
+// var responses = require('./responses');
 var admin = require('./admin');
 var interns = require('./interns');
 
 
 /** ---------- SUBROUTES ---------- **/
-// router.use('/calendar', calendar); // these are built off of the angular routes... but that is not what we want here
-// router.use('/admin-home', adminhome);
-// router.use('/admin-intern', interns);
-// router.use('/updateQuestions', updateq);
-// router.use('/importExport', importexport);
-// router.use('/feedbackform', feedback);
+
 
 
 router.use('/questions', questions);
-router.use('/responses', responses);
-router.use('/admin', admin);
-router.use('/interns', interns);
+// router.use('/responses', responses);
+// router.use('/admin', admin);
+// router.use('/interns', interns);
 
 
 /**
