7552b7315525547f08e70e31d7591977d41c6ec9
Matt Larson <matt.a.larson@gmail.com>
2017-07-01 09:41:14 -0500

    fixedSomeStuff

--- public/scripts/controllers/updateQuestionsController.js
+++ public/scripts/controllers/updateQuestionsController.js
@@ -1,9 +1,14 @@
 googleAuthApp.controller('updateQuestionsController', ['updateQuestionsService', function (updateQuestionsService) {
   console.log('loaded updateQuestionsController');
   const vm = this; // global variable
+
   vm.addQuestion = updateQuestionsService.addQuestion;// exporting addQuestion from updateQuestionsService.js
   vm.sendQuestion = updateQuestionsService.sendQuestion;// exporting sendQuestion from updateQuestionsService.js
   vm.questionCont = updateQuestionsService.questionCont;// exporting grabQuestion from updateQuestionsService.js
   vm.submitQuestion = updateQuestionsService.submitQuestion; // exporting submitQuestion from updateQuestionsService.js
   vm.clearInputs = updateQuestionsService.clearInputs;
+  vm.questionIn = updateQuestionsService.questionIn.question;
+
+  console.log('vm.questionIn=>', vm.questionIn);
+
 }]); //end updateQuestionsController
