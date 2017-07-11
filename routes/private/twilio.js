9d97f026cb7d68dcc0f19d7699a271989cfd0090
Jim Vang <vangjim15@gmail.com>
2017-07-10 20:16:46 -0500

    update twilio and updatequestion

--- routes/private/twilio.js
+++ routes/private/twilio.js
@@ -7,10 +7,10 @@ var pg = require('pg'); // require pg
 var twilio = require('twilio'); // require twilio
 var pool = require('../../modules/pool.js'); // create pool to connect to the database
 var env = require('dotenv').config();
+
+// Twilio info
 var ACCOUNTSID = process.env.accountSid;
 var AUTHTOKEN = process.env.authToken;
-
-// Twilio
 let client = new twilio(accountSid, authToken);
 
 router.get('/', function(req, res){
@@ -21,22 +21,15 @@ router.get('/', function(req, res){
       res.sendStatus(400);
     } //end if
     else {
+      // Twilio numbers
       let twilioNum = ["+17633346209", "+17633346219", "+17633346131", "+17633346557", "+17633346194", "+17637036066", "+17633249564", "+17637629952", "+17633346209"];
-      // NOTE Step-Up
-      // let batchSize = 200; // the first set of numbers
-      // let offsetCount = 0; // the end of the set
-      // NOTE PSI Testing
-      let batchSize = 2; // the first set of numbers
-      let offsetCount = 0; // the end of the set
+      let batchSize = 200; // the first set of numbers
+      let offsetCount = 0; // the end of the  first set
       for (let i = 0; i < twilioNum.length; i++) {
         // loop through the first 200 numbers and then the next 200
-        // NOTE Step-Up
-        // let resultSet = connection.query('SELECT phone FROM interns LIMIT $1 OFFSET $2', [batchSize, offsetCount], function(err, result){
-        // NOTE PSI Testing
-        let resultSet = connection.query('SELECT phone FROM psi LIMIT $1 OFFSET $2', [batchSize, offsetCount], function(err, result){
-          done();
+        let resultSet = connection.query('SELECT phone FROM interns LIMIT $1 OFFSET $2', [batchSize, offsetCount], function(err, result){
           console.log('result.rows->', result.rows);
-          psi(result.rows, twilioNum[i]);
+          phone(result.rows, twilioNum[i]);
         }); // end resultSet
         offsetCount += batchSize; // when batchSize increase offsetCount will also increase
       }// end FOR LOOP
@@ -44,13 +37,13 @@ router.get('/', function(req, res){
     } // end pool end else
 
     // NOTE Send SMS
-    let psi = function (newArray, fromNumber) {
+    let phone = function (newArray, fromNumber) {
     newArray.forEach(function(value){
       console.log('start of function ->', value);
       client.messages.create({
           to: value.phone, // value here to iterate phoneNumbers array
-          from: fromNumber, // registered Twilio account number
-          body: "This is your weekly STEP-UP check in! Please click the following link to take the very short survey. www.stepupcheckin.com Thank you!", // message to send
+          from: fromNumber, // registered Twilio account numbers
+          body: "This is your weekly STEP-UP check in! Please click the following link to take the very short survey. https://stepupcheckin.herokuapp.com/#/login Thank you!", // message to send and url to application
       }, function(err, message) {
         if (err) {
           console.log(err);
@@ -58,7 +51,7 @@ router.get('/', function(req, res){
         else {
           console.log('SMS message id ->', message.sid);
         } // end else
-      });// end Jim's client.message.create
+      });// end client.message.create
     });// end phoneNumbers iteration
   }; // end psi
 
